89ef57c71e1d01c3224a18b509c2021db7c946a9
Toru <toru.inoue@kissaki.tv>
2018-02-16 23:11:49 +0900

    add scene3

---
+++ Assets/SampleHandler3.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UUebView;
+
+public class SampleHandler3 : MonoBehaviour, IUUebViewEventHandler
+{
+    public void OnElementLongTapped(ContentType type, string param, string id)
+    {
+
+    }
+
+    public void OnElementTapped(ContentType type, GameObject element, string param, string id)
+    {
+
+    }
+
+    public void OnLoaded(string[] treeIds)
+    {
+
+    }
+
+    public void OnLoadFailed(ContentType type, int code, string reason)
+    {
+
+    }
+
+    public void OnLoadStarted()
+    {
+
+    }
+
+    public void OnProgress(double progress)
+    {
+
+    }
+
+    public void OnUpdated(string[] newTreeIds)
+    {
+
+    }
+}
